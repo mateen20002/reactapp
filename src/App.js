b2dc6ed4934650129dc15467d09e0c11fafafa9b
Mohammed Mateen Hussain Syed <mateen.syed2002@gmail.com>
2025-03-07 11:06:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.networkershome.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Join Networkers Home
         </a>
       </header>
     </div>
