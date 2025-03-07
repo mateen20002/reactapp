1d135cad9edc508b30c89763c47f4aca4dfa8111
Mohammed Mateen Hussain Syed <mateen.syed2002@gmail.com>
2025-03-07 11:09:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Networkershome</h1>
         <p>
-          Welcome to Networkershome.
+          Join us and be part of the community.
         </p>
         <a
           className="App-link"
@@ -17,6 +18,14 @@ function App() {
         >
           Join Networkers Home
         </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> {/* Added a "Learn React" link */}
       </header>
     </div>
   );
